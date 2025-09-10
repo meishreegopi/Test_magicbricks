ecd61f05396fdbe017cc51973bb82cffa19e45ea
meishreegopi <meishreegopi1@gmail.com>
2025-09-10 12:03:52 +0530

    Day-5 Shortlisting and Report Screenshot works successful

--- src/test/java/com/pages/SearchHomePage.java
+++ src/test/java/com/pages/SearchHomePage.java
@@ -1,15 +1,19 @@
 package com.pages;
 
 import java.time.Duration;
+
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.objectrepository.Locators;
+
 import com.setup.Reporter;
 
 
--- src/test/java/com/setup/Reporter.java
+++ src/test/java/com/setup/Reporter.java
@@ -1,7 +1,7 @@
 package com.setup;
 
 import java.io.File;
-import java.io.IOException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -16,26 +16,53 @@ import com.aventstack.extentreports.Status;
 
 public class Reporter {
 
-    public static void generateReport(WebDriver driver, ExtentTest test, Status status, String stepDetails) {
-        try {
-            if (status == Status.FAIL) {
-                String screenshotPath = captureScreenshot(driver);
-                test.log(status, stepDetails,
-                        MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
-            } else {
-                test.log(status, stepDetails);
+    public static void generateReport(WebDriver driver, ExtentTest extTest, Status status, String stepMessage) {
+        if (extTest == null) {
+            System.out.println("ExtentTest object is null. Skipping report logging.");
+            return;
+        }
+
+        if (status.equals(Status.PASS)) {
+            System.out.println(" ******* " + stepMessage + " is passed");
+            extTest.log(status, stepMessage);
+        } else if (status.equals(Status.FAIL)) {
+            System.out.println("***************** step is failed");
+            String screenshotPath = null;
+
+            // take screenshot only if driver is valid
+            if (driver != null) {
+                screenshotPath = captureScreenshot(driver, stepMessage);
             }
-        } catch (Exception e) {
-            test.log(Status.WARNING, "Reporting failed: " + e.getMessage());
+
+            try {
+                if (screenshotPath != null) {
+                    extTest.log(status, stepMessage,
+                            MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+                } else {
+                    extTest.log(status, stepMessage + " (No screenshot - driver was null)");
+                }
+            } catch (Exception e) {
+                extTest.log(status, stepMessage + " (Screenshot capture failed: " + e.getMessage() + ")");
+            }
         }
     }
 
-    private static String captureScreenshot(WebDriver driver) throws IOException {
-        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        String screenshotPath = "screenshots/" +
-                new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".png";
-        File dest = new File(screenshotPath);
-        FileUtils.copyFile(src, dest);
-        return screenshotPath;
+    public static String captureScreenshot(WebDriver driver, String errorMessage) {
+        String userDir = System.getProperty("user.dir");
+
+        // timestamped file name
+        String dateTime = new SimpleDateFormat("dd-MM-yyyy_HH_mm_ss").format(new Date());
+        String fileName = userDir + "\\screenshots\\" + errorMessage.replaceAll("[^a-zA-Z0-9]", "_") 
+                          + "_" + dateTime + ".png";
+
+        try {
+            File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            File destFile = new File(fileName);
+            FileUtils.copyFile(srcFile, destFile);
+            return fileName;
+        } catch (Exception e) {
+            System.out.println(" Failed to capture screenshot: " + e.getMessage());
+            return null;
+        }
     }
 }
